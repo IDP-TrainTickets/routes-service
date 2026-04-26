9e06eaa9c1b77f77b7c18503f5ef1dcccac023ef
Andrei Stan <andreistan2003@gmail.com>
2026-04-26 17:30:58 +0300

    Add ORM schemas and sample routes

---
+++ app.py
@@ -0,0 +1,136 @@
+from typing import List, Tuple
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import UniqueConstraint
+from sqlalchemy.orm import RelationshipProperty
+import jwt
+
+app = Flask(__name__)
+
+# Testing values
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:password123@db:5432/db'
+app.config['SECRET_KEY'] = 'secret-key-test-123'
+
+
+db = SQLAlchemy(app)
+
+
+def get_requesting_user() -> Tuple[str, str] | None:
+    auth_header = request.headers.get("Authorization")
+
+    if not auth_header:
+        return None
+
+    token = auth_header.removeprefix('Bearer ').strip()
+    claims = jwt.decode(token, options={'verify_signature': False})
+    return claims['user_id'], claims['user_type']
+
+
+def transform_price(price: int, user_type: str) -> int:
+    if user_type == "Student":
+        return int(price * 0.9)
+    return price
+
+
+class User(db.Model):
+    __tablename__ = "app_user"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    user_type = db.Column(db.String(20), default='Adult')
+
+
+class Route(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    from_station = db.Column(db.String(255), nullable=False)
+    to_station = db.Column(db.String(255), nullable=False)
+    leave_at = db.Column(db.Time, nullable=False)
+    arrive_by = db.Column(db.Time, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+
+    trips = db.relationship("Trip", back_populates="route")
+
+
+class Trip(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    route_id = db.Column(
+        db.Integer,
+        db.ForeignKey("route.id"),
+        nullable=False,
+    )
+    trip_day = db.Column(db.Date, nullable=False)
+    capacity = db.Column(db.Integer, nullable=False)
+    route: RelationshipProperty[Route]= db.relationship("Route", back_populates="trips")
+    reservations = db.relationship("Reservation", back_populates="trip")
+
+
+class Reservation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    trip_id = db.Column(
+        db.Integer,
+        db.ForeignKey("trip.id"),
+        nullable=False,
+    )
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("user.id"),
+        nullable=False,
+    )
+    trip: RelationshipProperty[Trip] = db.relationship("Trip", back_populates="reservations")
+    user: RelationshipProperty[User] = db.relationship("User", back_populates="reservations")
+
+
+# example: /routes?from_station=Bucharest"
+@app.get('/routes')
+def list_routes():
+    user = get_requesting_user()
+    if not user:
+        return {"error": "Could not identify requesting user"}, 403
+    
+    _, user_type = user
+    
+    from_station = request.args.get("from_station")
+    query = Route.query
+
+    if from_station is not None:
+        query = query.filter(Route.from_station == from_station)
+
+    routes = query.all()
+
+    data = [{
+        "from_station": route.from_station,
+        "to_station": route.to_station,
+        "leave_at": route.leave_at.strftime("%H:%M:%S"),
+        "arrive_by": route.arrive_by.strftime("%H:%M:%S"),
+        "price": transform_price(route.price, user_type),
+    }
+    for route in routes]
+
+    return jsonify(data), 200
+
+@app.get('/reservations')
+def list_reservations():
+    user = get_requesting_user()
+    if not user:
+        return {"error": "Could not identify requesting user"}, 403
+    
+    user_id, _ = user
+ 
+    query = Reservation.query
+    query = query.filter(Reservation.user_id == user_id)
+    reservations: List[Reservation]= query.all()
+
+    data = [{
+        "from_station": reservation.trip.route.from_station,
+        "to_station": reservation.trip.route.to_station,
+        "leave_at": reservation.trip.route.leave_at.strftime("%H:%M:%S"),
+        "arrive_by": reservation.trip.route.arrive_by.strftime("%H:%M:%S"),
+        "day": reservation.trip.trip_day
+    }
+    for reservation in reservations]
+
+    return jsonify(data), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
